57b021dff000bdedf48f9638a35e0ff507e8551d
fikerte <tofikertesfaye@gmail.com>
2021-08-30 17:30:28 +0300

    Add menu links to the correct page

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var hamburguer = document.querySelector('.ham-menu');
 var mobile = document.querySelector('.mobile-hidden');
 var close = document.querySelector('.close');
 var body = document.querySelector('body');
+var portfolio = document.querySelector('.portfolio');
+var about = document.querySelector('.mobile-about');
+var contact = document.querySelector('.mobile-contact');
+
 hamburguer.addEventListener('click', open);
 function open() {
   mobile.style.left = '0';
@@ -13,3 +17,9 @@ function closeMenu() {
   mobile.style.left = '100%';
   body.style.overflow = 'auto';
 }
+
+portfolio.addEventListener('click', closeMenu);
+about.addEventListener('click', closeMenu);
+contact.addEventListener('click', closeMenu);
+
+
