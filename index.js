0d7db0566a826fa9d80f163189f5fef02f1c027a
fikerte <tofikertesfaye@gmail.com>
2021-09-01 17:46:53 +0300

    Fix some linter errors

--- index.js
+++ index.js
@@ -5,25 +5,22 @@ const body = document.querySelector('body');
 const portfolio = document.querySelector('.portfolio');
 const about = document.querySelector('.mobile-about');
 const contact = document.querySelector('.mobile-contact');
-const html = document.querySelector('html');
 
-mobile.style = "display: none; "
+mobile.style = 'display: none';
 function open() {
   mobile.style.display = 'block';
   body.style.overflow = 'hidden';
-  //set timeout to open menu
-  setTimeout(function() {
+  // set timeout to open menu
+  setTimeout(() => {
     mobile.style.left = '0';
-  }
-  , 100);
+  }, 100);
 }
 
 function closeMenu() {
-  //set timeout to close menu
-  setTimeout(function() {
+  // set timeout to close menu
+  setTimeout(() => {
     mobile.style.display = 'none';
-  }
-  , 200);
+  }, 200);
   body.style.overflow = 'auto';
   mobile.style.left = '100%';
 }
--- popup.js
+++ popup.js
@@ -1,82 +1,80 @@
-//create the variables we are going to use
-const bodySelector = document.querySelector('body');
+// create the variables we are going to use
 const mainModalContainer = document.createElement('div');
-const closeCross = document.createElement('p');
 const firstCardBtn = document.querySelector('.obj-btn-1');
 const secondCardBtn = document.querySelector('.obj-btn-2');
 const thirdCardBtn = document.querySelector('.obj-btn-3');
 const fourthCardBtn = document.querySelector('.obj-btn-4');
 
-//create variables for images
-var firstCardImage = document.createElement('img');
-firstCardImage.className = "image obj-image-1";
-firstCardImage.alt = "project example"
-var secondCardImage = document.createElement('img');
-secondCardImage.className = "image obj-image-2";
-secondCardImage.alt = "project example"
-var thirdCardImage = document.createElement('img');
-thirdCardImage.className = "image obj-image-3";
-thirdCardImage.alt = "project example"
-var fourthCardImage = document.createElement('img');
-fourthCardImage.className = "image obj-image-4";
-fourthCardImage.alt = "project example"
-var imgCounter1 = document.createElement('img');
-var imgCounter2 = document.createElement('img');
+// create variables for images
+const firstCardImage = document.createElement('img');
+firstCardImage.className = 'image obj-image-1';
+firstCardImage.alt = 'project example';
+const secondCardImage = document.createElement('img');
+secondCardImage.className = 'image obj-image-2';
+secondCardImage.alt = 'project example';
+const thirdCardImage = document.createElement('img');
+thirdCardImage.className = 'image obj-image-3';
+thirdCardImage.alt = 'project example';
+const fourthCardImage = document.createElement('img');
+fourthCardImage.className = 'image obj-image-4';
+fourthCardImage.alt = 'project example';
+const imgCounter1 = document.createElement('img');
+const imgCounter2 = document.createElement('img');
 
-//create variables for the links we are going to use
-const SourceLink = "https://github.com/Lordkaito/Personal-portfolio";
-const LiveLink = "https://lordkaito.github.io/Personal-portfolio/";
+// create variables for the links we are going to use
+const SourceLink = 'https://github.com/Lordkaito/Personal-portfolio';
+const LiveLink = 'https://lordkaito.github.io/Personal-portfolio/';
 
-//create the objects we are going to use
-var firstProject = {
-  "name": "Tonic",
-  "frame": ["CANOPY", "Back end Dev", 2015],
-  "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  "featuredImage": firstCardImage.src = "./images/work-4.png",
-  "technologies": ["Html", "Css", "JavaScript"],
-  "liveLink": LiveLink,
-  "sourceLink": SourceLink,
-  "frameImg": [imgCounter1.src = "./images/Counter.png", imgCounter2.src = "./images/Counter.png"],
-  "scrContainer": "scr-container1",
-}
+// create the objects we are going to use
+const firstProject = {
+  name: 'Tonic',
+  frame: ['CANOPY', 'Back end Dev', 2015],
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: firstCardImage.src = './images/work-4.png',
+  technologies: ['Html', 'Css', 'JavaScript'],
+  liveLink: LiveLink,
+  sourceLink: SourceLink,
+  frameImg: [imgCounter1.src = './images/Counter.png', imgCounter2.src = './images/Counter.png'],
+  scrContainer: 'scr-container1',
+};
 
-var secondProject = {
-  "name": "Multi-Post Stories",
-  "frame": ["CANOPY", "Back end Dev", 2015],
-  "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  "featuredImage": secondCardImage.src = "./images/work-3.png",
-  "technologies": ["Html", "Css", "JavaScript"],
-  "liveLink": LiveLink,
-  "sourceLink": SourceLink,
-  "frameImg": [imgCounter1.src = "./images/Counter.png", imgCounter2.src = "./images/Counter.png"],
-}
+const secondProject = {
+  name: 'Multi-Post Stories',
+  frame: ['CANOPY', 'Back end Dev', 2015],
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: secondCardImage.src = './images/work-3.png',
+  technologies: ['Html', 'Css', 'JavaScript'],
+  liveLink: LiveLink,
+  sourceLink: SourceLink,
+  frameImg: [imgCounter1.src = './images/Counter.png', imgCounter2.src = './images/Counter.png'],
+};
 
-var thirdProject = {
-  "name": "Toni",
-  "frame": ["CANOPY", "Back end Dev", 2015],
-  "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  "featuredImage": thirdCardImage.src = "./images/work-2.png",
-  "technologies": ["Html", "Css", "JavaScript"],
-  "liveLink": LiveLink,
-  "sourceLink": SourceLink,
-  "frameImg": [imgCounter1.src = "./images/Counter.png", imgCounter2.src = "./images/Counter.png"],
-}
+const thirdProject = {
+  name: 'Toni',
+  frame: ['CANOPY', 'Back end Dev', 2015],
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: thirdCardImage.src = './images/work-2.png',
+  technologies: ['Html', 'Css', 'JavaScript'],
+  liveLink: LiveLink,
+  sourceLink: SourceLink,
+  frameImg: [imgCounter1.src = './images/Counter.png', imgCounter2.src = './images/Counter.png'],
+};
 
-var fourthProject = {
-  "name": "Multi-Post Stories",
-  "frame": ["CANOPY", "Back end Dev", 2015],
-  "description": "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-  "featuredImage": fourthCardImage.src = "./images/work-1.png",
-  "technologies": ["Html", "Css", "JavaScript"],
-  "liveLink": LiveLink,
-  "sourceLink": SourceLink,
-  "frameImg": [imgCounter1.src = "./images/Counter.png", imgCounter2.src = "./images/Counter.png"],
-}
+const fourthProject = {
+  name: 'Multi-Post Stories',
+  frame: ['CANOPY', 'Back end Dev', 2015],
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  featuredImage: fourthCardImage.src = './images/work-1.png',
+  technologies: ['Html', 'Css', 'JavaScript'],
+  liveLink: LiveLink,
+  sourceLink: SourceLink,
+  frameImg: [imgCounter1.src = './images/Counter.png', imgCounter2.src = './images/Counter.png'],
+};
 
-var arr = [firstProject, secondProject, thirdProject, fourthProject];
+const arr = [firstProject, secondProject, thirdProject, fourthProject];
 
-//fucntion that loads once the page is loaded
-window.onload = function () {
+// fucntion that loads once the page is loaded
+window.onload = () => {
   document.querySelector('.obj-name-first').innerHTML = firstProject.name;
   document.querySelector('.obj-frame-1-first').innerHTML = firstProject.frame[0];
   // document.querySelector('.frame-img-1-first').appendChild(imgCounter1);
@@ -89,7 +87,7 @@ window.onload = function () {
   document.querySelector('.obj-tech-3-first').innerHTML = firstProject.technologies[2];
   document.querySelector('.image-card-first').appendChild(firstCardImage);
 
-  //second project
+  // second project
   document.querySelector('.image-card-second').appendChild(secondCardImage);
   document.querySelector('.obj-name-second').innerHTML = secondProject.name;
   document.querySelector('.obj-frame-1-second').innerHTML = secondProject.frame[0];
@@ -102,7 +100,7 @@ window.onload = function () {
   document.querySelector('.obj-tech-2-second').innerHTML = secondProject.technologies[1];
   document.querySelector('.obj-tech-3-second').innerHTML = secondProject.technologies[2];
 
-  //third project
+  // third project
   document.querySelector('.obj-name-third').innerHTML = thirdProject.name;
   document.querySelector('.obj-frame-1-third').innerHTML = thirdProject.frame[0];
   // document.querySelector('.frame-img-1-third').appendChild(imgCounter1);
@@ -115,7 +113,7 @@ window.onload = function () {
   document.querySelector('.obj-tech-3-third').innerHTML = thirdProject.technologies[2];
   document.querySelector('.image-card-third').appendChild(thirdCardImage);
 
-  //fourth project
+  // fourth project
   document.querySelector('.obj-name-fourth').innerHTML = fourthProject.name;
   document.querySelector('.obj-frame-1-fourth').innerHTML = fourthProject.frame[0];
   // document.querySelector('.frame-img-1-fourth').appendChild(imgCounter1);
@@ -127,73 +125,71 @@ window.onload = function () {
   document.querySelector('.obj-tech-2-fourth').innerHTML = fourthProject.technologies[1];
   document.querySelector('.obj-tech-3-fourth').innerHTML = fourthProject.technologies[2];
   document.querySelector('.image-card-fourth').appendChild(fourthCardImage);
-}
+};
 
-
-//for the onload function
+// for the onload function
 
 // let workSection = document.querySelector('#work-section');
 // mainModalContainer.innerHTML = ``;
 
-//basic style for the modal
-mainModalContainer.style = "transform: scale(0); background-color: white; max-width: 100%; width: 100%; position: absolute; top: -20%; left: 0; right: 0; bottom: 0; transition: all 0.5s ease-in-out; border-radius: 20px; box-shadow: #7a869a 0 0 10px;";
-mainModalContainer.className = "modal-container";
+// basic style for the modal
+mainModalContainer.style = 'transform: scale(0); background-color: white; max-width: 100%; width: 100%; position: absolute; top: -20%; left: 0; right: 0; bottom: 0; transition: all 0.5s ease-in-out; border-radius: 20px; box-shadow: #7a869a 0 0 10px;';
+mainModalContainer.className = 'modal-container';
 
-let workCard = document.querySelectorAll(".scr-container");
-workCard.forEach(function (card) {
-  card.classList.add("relative");
-  card.style = "position: relative;";
+const workCard = document.querySelectorAll('.scr-container');
+workCard.forEach((card) => {
+  card.classList.add('relative');
+  card.style = 'position: relative;';
 });
 
 function modalObj(value) {
-  mainModalContainer.style.transform = "scale(1)";
+  mainModalContainer.style.transform = 'scale(1)';
   mainModalContainer.innerHTML = `
-  <div class="modal-header">
-    <div class="modal-title">
+  <div class='modal-header'>
+    <div class='modal-title'>
       <h1>${value.name}</h1>
-      <div class="modal-frame">
+      <div class='modal-frame'>
         <h3>${value.frame[0]}</h3>
-        <img src="${value.frameImg[0]}" alt="frame">
+        <img src='${value.frameImg[0]}' alt='frame'>
         <h3>${value.frame[1]}</h3>
-        <img src="${value.frameImg[1]}" alt="frame">
+        <img src='${value.frameImg[1]}' alt='frame'>
         <h3>${value.frame[2]}</h3>
       </div>
     </div>
-    <a class="close-modal"> &times; </a>
+    <a class='close-modal'> &times; </a>
   </div>
-  <div class="modal-image">
-    <img src="${value.featuredImage}" alt="${value.name}">
+  <div class='modal-image'>
+    <img src='${value.featuredImage}' alt='${value.name}'>
   </div>
-  <div class="modal-description">
+  <div class='modal-description'>
     <p>${value.description}</p>
-    <div class="modal-tech-link-container">
-      <div class="modal-tech">
+    <div class='modal-tech-link-container'>
+      <div class='modal-tech'>
         <ul>
-          <li class="tag">${value.technologies[0]}</li>
-          <li class="tag">${value.technologies[1]}</li>
-          <li class="tag">${value.technologies[2]}</li>
+          <li class='tag'>${value.technologies[0]}</li>
+          <li class='tag'>${value.technologies[1]}</li>
+          <li class='tag'>${value.technologies[2]}</li>
         </ul>
       </div>
-      <div class="modal-links">
-        <a href="${value.githubLink}" target="_blank" class="action-btn">Github</a>
-        <a href="${value.liveLink}" target="_blank" class="action-btn">Live</a>
+      <div class='modal-links'>
+        <a href='${value.githubLink}' target='_blank' class='action-btn'>Github</a>
+        <a href='${value.liveLink}' target='_blank' class='action-btn'>Live</a>
       </div>
     </div>
   </div>`;
 
-  if (value == arr[0]) {
+  if (value === arr[0]) {
     workCard[0].appendChild(mainModalContainer);
-  } else if (value == arr[1]) {
+  } else if (value === arr[1]) {
     workCard[1].appendChild(mainModalContainer);
-  } else if (value == arr[2]) {
+  } else if (value === arr[2]) {
     workCard[2].appendChild(mainModalContainer);
-  } else if (value == arr[3]) {
+  } else if (value === arr[3]) {
     workCard[3].appendChild(mainModalContainer);
   }
-  
-  let close = document.querySelector('.close-modal');
-  close.addEventListener('click', function () {
-    mainModalContainer.style.transform = "scale(0)" ;
+  const close = document.querySelector('.close-modal');
+  close.addEventListener('click', () => {
+    mainModalContainer.style.transform = 'scale(0)';
   });
 }
 
